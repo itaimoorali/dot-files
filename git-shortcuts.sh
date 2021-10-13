e11f1ed0a597b34a22adaba7bded43f1832a8006
Taimoor Ali <taimoor736@gmail.com>
2021-10-13 13:28:40 +0500

    chore: savepoint

--- git-shortcuts.sh
+++ git-shortcuts.sh
@@ -1,2 +1,10 @@
 alias gcmer='git branch -d `git branch --merged`';
 alias g-bcp="git branch | grep '^\*' | cut -d' ' -f2 | tr -d '\n' | pbcopy"
+
+
+## Show branch name in the terminal
+git_branch() {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+
+export PS1="\W \e[1;33m ~ \$(git_branch)\e[0m \$ "
