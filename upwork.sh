eb883bba87be6d748f87ddb43dde6d72ea2dfda0
Taimoor Ali <taimoorali@cloud.upwork.com>
2022-01-28 17:30:13 +0500

    update

--- upwork.sh
+++ upwork.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Git ShortCuts
 alias up-setgitcfg='git config user.name "Taimoor Ali" && git config user.email "taimoorali@cloud.upwork.com"';
-alias up-gtmatch='cd ~/work/office/upwork/match-team';
+alias up-gtmatch='cd ~/work/upwork/match-team';
 
 source ~/dot-files/up_setup_node.sh;
