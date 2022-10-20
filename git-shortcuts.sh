517894d54893d562d82ec5517aedaa79644b6efa
Taimoor Ali <taimoor736@gmail.com>
2022-10-20 20:27:26 +0500

    Add script for killing process.

--- git-shortcuts.sh
+++ git-shortcuts.sh
@@ -1,6 +1,11 @@
-alias gcmer='git branch -d `git branch --merged`';
+alias gcmer='git branch --merged | egrep -v "master|develop" | xargs git branch -d';
+alias gcFlushBranch='git branch | grep -v "master\|develop" | xargs git branch -D';
 alias g-bcp="git branch | grep '^\*' | cut -d' ' -f2 | tr -d '\n' | pbcopy"
-
+alias checkout-develop="git checkout develop";
+alias checkout-master="git checkout master";
+alias pull-develop="git pull origin develop";
+alias pull-master="git pull origin master";
+alias gdateupdate="git commit --amend --no-edit --date \"$(date)\""
 
 ## Show branch name in the terminal
 git_branch() {
@@ -8,3 +13,17 @@ git_branch() {
 }
 
 export PS1="\W \e[1;33m ~ \$(git_branch)\e[0m \$ "
+
+## Remove all the branches except master and develop
+git_flush_branches() {
+  read -p $'\e[91mThis will delete all Git branches except master and develop. Are you sure you want to proceed? (y/n): \e[0m' answer
+
+  if [[ "$answer" =~ ^[Yy]$ ]]; then
+    git branch | grep -v "master\|develop" | xargs git branch -D
+    echo "Branches deleted successfully."
+  else
+    echo "Operation canceled."
+  fi
+}
+
+
--- upwork.sh
+++ upwork.sh
@@ -4,3 +4,17 @@ alias up-setgitcfg='git config user.name "Taimoor Ali" && git config user.email 
 alias up-gtmatch='cd ~/work/upwork/match-team';
 
 source ~/dot-files/up_setup_node.sh;
+
+match-a-venv() {
+  CURRENT_DIR=$(pwd);
+
+  cd ~/work/upwork/match-team;
+  a-venv;
+  echo $CURRENT_DIR;
+  cd "$CURRENT_DIR";
+}
+
+# Get the certificate (.crt) from BookMark Name (Upwork/Upwork Global Root CA Cert) 
+# and put in /etc/ssl/certs directory
+##
+export NODE_EXTRA_CA_CERTS='/etc/ssl/certs/upwork-global-root-ca.crt'
